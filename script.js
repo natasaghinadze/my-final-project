bb66cc963bfd29de92400bae3e4e9ea7ec4e74bd
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2021-07-30 05:42:51 -0700

    added changes and (script.js)

---
+++ script.js
@@ -0,0 +1,6 @@
+let naviganionnn = document.getElementById('allnav')
+let burger = document.getElementById('burger-bar')
+
+burger.addEventListener('click', function() {
+    naviganionnn.classList.toggle('on');
+})
